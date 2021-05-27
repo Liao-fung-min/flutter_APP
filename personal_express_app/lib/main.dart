ac5113872d82fe94911984c26014db1dc0f5fc49
DESKTOP-SOD8SE9\Alex <rice0037308@gmail.com>
2021-05-27 23:13:51 +0800

    fix about CRUD function

--- personal_express_app/lib/main.dart
+++ personal_express_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import './transaction.dart';
+
+import './widgets/user_transactions.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,24 +15,11 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transcation = [
-    Transaction(
-      id: 'tl',
-      title: '衣服',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: '鞋子',
-      amount: 57.25,
-      date: DateTime.now(),
-    ),
-  ];
-  // String titleInput; //名稱輸入
-  // String amountInput; //價錢輸入
-  final titlController = TextEditingController();
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
   final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,93 +27,18 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
-        //mainAxisAlignment: MainAxisAlignment.end,
+        // mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
             width: double.infinity,
             child: Card(
-              color: Colors.orange,
-              child: Text('Chart!'),
-              elevation: 20,
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
             ),
           ),
-          Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(labelText: '品項'),
-                    controller: titlController,
-                    //onChanged: (val) => amountInput = val,
-                    //另一種寫法
-                    // onChanged: (val) {
-                    //   titleInput = val;
-                    // },
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: '價錢'),
-                    controller: amountController,
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      print(titlController.text);
-                      print(amountController.text);
-                    },
-                    child: Text('增加項目'),
-                    textColor: Colors.purple,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Column(
-            children: transcation.map((tx) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    //針對container裡面的字做css的設計
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.pink, width: 1),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\$${tx.amount}', //錢字號寫法
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      //向左靠齊
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          DateFormat.yMMMMd().format(tx.date), //日期寫法
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
+          UserTransactions()
         ],
       ),
     );
